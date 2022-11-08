bcc1f4bf3b1e6727a8ff8de2212f79da8486e48a
movilidadagil <testerselenium34@gmail.com>
2022-11-08 22:28:16 +0300

    pipeline

---
+++ src/test/java/framework/DriverSetup.java
@@ -0,0 +1,60 @@
+package framework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class DriverSetup {
+
+    static WebDriver driver;
+    static Properties properties;
+
+    public static WebDriver initialize_Driver(String browser) throws MalformedURLException {
+
+        //Chrome options for register-login pages
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled"); //removes the detection
+        options.addArguments("--disable-notifications");
+        options.setExperimentalOption("excludeSwitches", "disable-popup-blocking");
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"}); //removes the banner
+        properties = ConfigReader.getProperties();
+        if(browser.equals("firefox-remote")){
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            WebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1"), firefoxOptions);
+        }
+        if (browser.equals("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(options);
+        } else if (browser.equals("Firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(options);
+        }
+
+        String url = properties.getProperty("url");
+        int impWait = Integer.parseInt(properties.getProperty("implicityWait"));
+        int pageWait = Integer.parseInt(properties.getProperty("pageLoadTimeout"));
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(impWait, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(pageWait, TimeUnit.SECONDS);
+        return getDriver();
+    }
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
+
+
+}
