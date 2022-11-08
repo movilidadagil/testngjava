6db1d5e507130b7408e6f9f371f2da44185cd661
movilidadagil <testerselenium34@gmail.com>
2022-11-08 22:52:56 +0300

    fixes

--- src/test/java/framework/DriverSetup.java
+++ src/test/java/framework/DriverSetup.java
@@ -21,17 +21,18 @@ public class DriverSetup {
     public static WebDriver initialize_Driver(String browser) throws MalformedURLException {
 
         //Chrome options for register-login pages
-        ChromeOptions options = new ChromeOptions();
+        /*ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-blink-features=AutomationControlled"); //removes the detection
         options.addArguments("--disable-notifications");
         options.setExperimentalOption("excludeSwitches", "disable-popup-blocking");
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"}); //removes the banner
+        */
         properties = ConfigReader.getProperties();
         if(browser.equals("firefox-remote")){
             FirefoxOptions firefoxOptions = new FirefoxOptions();
             driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444"), firefoxOptions);
         }
-        if (browser.equals("Chrome")) {
+        /*if (browser.equals("Chrome")) {
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver(options);
         } else if (browser.equals("Firefox")) {
@@ -40,15 +41,15 @@ public class DriverSetup {
         } else {
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver(options);
-        }
+        }*/
 
         String url = properties.getProperty("url");
         int impWait = Integer.parseInt(properties.getProperty("implicityWait"));
         int pageWait = Integer.parseInt(properties.getProperty("pageLoadTimeout"));
         driver.get(url);
-        driver.manage().window().maximize();
+        /*driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(impWait, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(pageWait, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(pageWait, TimeUnit.SECONDS);*/
         return getDriver();
     }
 
